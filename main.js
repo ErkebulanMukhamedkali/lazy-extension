82bb79897ee75cf61df5ada2192f4d5bb2503cd7
YerkebulanMukhamedkali <erkebulan.mukhamedkali@gmail.com>
2023-01-18 11:31:26 +0600

    modified due to autoplay button nerf from website

--- main.js
+++ main.js
@@ -2,27 +2,32 @@ const skipOpeningClass = "vjs-overlay-bottom-left"
 const nextEpisodeClass = "vjs-overlay-bottom-right"
 const autoPlayClass = "vjs-big-play-button"
 
-let autoPlayButton = document.querySelector(`.${autoPlayClass}`)
-autoPlayButton.click()
+let bigObserver = new MutationObserver(function(mutations, obs) {
+    let autoPlayButton = document.querySelector(`.${autoPlayClass}`)
+    if (autoPlayButton) autoPlayButton.click();
 
-// Focus on video for "F" button work
-document.querySelector('#my-player_html5_api').focus()
+    let skipOpeningButton = document.querySelector(`.${skipOpeningClass}`)
+    let nextEpisodeButton = document.querySelector(`.${nextEpisodeClass}`)
+    if (skipOpeningButton && nextEpisodeButton) {
+        setSkipObserver();
+        obs.disconnect();
+        setSkipObserver();
+        return;
+    }
+})
+bigObserver.observe(document, { childList: true, subtree: true });
 
-// const fullScreenClass = "vjs-fullscreen-control"
-// let fullScreenButton = document.querySelector(`.${fullScreenClass}`)
-// if(document.fullscreenElement === null) {
-//     fullScreenButton.click()
-// }
+function setSkipObserver() {
+    let skipOpeningButton = document.querySelector(`.${skipOpeningClass}`)
+    let nextEpisodeButton = document.querySelector(`.${nextEpisodeClass}`)
 
-let skipOpeningButton = document.querySelector(`.${skipOpeningClass}`)
-let nextEpisodeButton = document.querySelector(`.${nextEpisodeClass}`)
+    let observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.attributeName !== 'class') return;
+            if (!mutation.target.classList.contains('vjs-hidden')) mutation.target.click();
+        })
+    });
 
-let observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-    if (mutation.attributeName !== 'class') return;
-    if (!mutation.target.classList.contains('vjs-hidden')) mutation.target.click();
-    })
-});
-
-observer.observe(skipOpeningButton, { attributes: true });
-observer.observe(nextEpisodeButton, { attributes: true });
+    observer.observe(skipOpeningButton, { attributes: true });
+    observer.observe(nextEpisodeButton, { attributes: true });
+}
